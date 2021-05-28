42722b84c0ba0e36b09e37b761017c24ad815ac2
hudson88@yandex.ru <hudson88@yandex.ru>
2021-05-28 10:22:55 +0300

    added header

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,7 @@
 /* libs */
 
 //= libs/jquery-3.3.1.min.js
-//= libs/jquery.fancybox.min.js
 //= libs/slick.min.js
 
 /* my scripts */
-
-//= partials/components.js
-//= partials/header.js
 //= partials/main.js
--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,5 +1,2 @@
-$(document).ready(function () {
-   // $("input[name='phone']").mask(" +7 (999) 999-99-99");
 
-});
 
