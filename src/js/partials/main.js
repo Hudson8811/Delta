dd900e4c9a05c8f91f591880e8bfbc9347171d40
hudson88@yandex.ru <hudson88@yandex.ru>
2021-05-31 14:01:38 +0300

    refact main.js

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,50 +1,175 @@
-document.addEventListener('DOMContentLoaded', function() {
-
+document.addEventListener("DOMContentLoaded", function () {
 	const switchHomeImages = () => {
-		const buttonsWrapper = document.querySelector('.home-content__icons');
-		const backgroundImages = document.querySelectorAll('.overlay>img');
-		
+		const buttonsWrapper = document.querySelector(".home-content__icons");
+		const backgroundImages = document.querySelectorAll(".overlay>img");
+
 		if (!buttonsWrapper) return;
 
 		let isHovered = false;
 
-		buttonsWrapper.addEventListener('mouseover', event => {
+		buttonsWrapper.addEventListener("mouseover", (event) => {
 			if (!isHovered) {
 				let target = event.target;
 
-				target = target.closest('.home-content__icons-item');
-	
+				target = target.closest(".home-content__icons-item");
+
 				if (target) {
 					isHovered = true;
 					backgroundImages.forEach((image, index) => {
-						image.className = +target.dataset.item === index ? 'active' : '';
+						image.className = +target.dataset.item === index ? "active" : "";
 					});
 
-					target.addEventListener('mouseleave', () => {
+					target.addEventListener("mouseleave", () => {
 						isHovered = false;
 					});
 				}
 			}
 		});
-	}
+	};
 
 	switchHomeImages();
 
+	const nextSlide = tickSlide();
+
 	const startGame = () => {
-		document.body.classList.add('game-is-on');
+		// const form = document.querySelector("form");
+		const timer = startTimer();
+
+		let livesCount = 3;
+
+		document.body.classList.add("game-is-on");
+
+		document.addEventListener("change", (e) => {
+			let target = e.target;
+
+			if (target.value === "fail" && livesCount > 0) {
+				const invalidBlock = target.closest(".game__form-question");
+
+				livesCount--;
+				livesDecrease(livesCount);
+				showError(invalidBlock);
+			}
+
+			if (livesCount === 0) {
+				gameover(timer);
+			}
+
+			if (target.value === "correct") {
+				hideError();
+				nextSlide();
+			}
+		});
+	};
+
+	function startTimer() {
+		const timer = document.getElementById("timer");
+		const base = "60";
+
+		let d = "00";
+		let h = "00";
+		let min = "00";
+		let sec = "00";
+
+		const tick = () => {
+			sec++;
+
+			if (sec < "10") {
+				sec = "0" + sec;
+			}
+
+			if (sec === +base) {
+				sec = "00";
+				min++;
+				if (min < "10") {
+					min = "0" + min;
+				}
+			}
+
+			if (min === base) {
+				min = "00";
+				h++;
+				if (h < "10") {
+					h = "0" + h;
+				}
+			}
+
+			if (h === "24") {
+				h = "00";
+				d++;
+				if (d < "10") {
+					d = "0" + d;
+				}
+			}
+
+			timer.textContent = `${d}:${h}:${min}:${sec}`;
+		};
+
+		const timerInterval = setInterval(tick, 1000);
+
+		return timerInterval;
 	}
+
+	function stopTimer(timer) {
+		clearInterval(timer);
+	}
+
+	function gameover(timer) {
+		const gameSection = document.querySelector(".game");
+
+		gameSection.classList.add("game--finished");
+
+		stopTimer(timer);
+	}
+
+	function livesDecrease(livesCount) {
+		const livesMobileIcons = document.querySelector(".game__lives");
+		const livesDesktopIcons = document.querySelector(".footer__lives");
+
+		const livesMobile = livesMobileIcons.querySelectorAll("img");
+		const livesDesktop = livesDesktopIcons.querySelectorAll("img");
+
+		const ICON_SRC = "img/heart.svg";
+
+		livesMobile[livesCount].src = ICON_SRC;
+		livesDesktop[livesCount].src = ICON_SRC;
+	}
+
+	function showError(currentInvalidBlock) {
+		hideError();
+
+		currentInvalidBlock.classList.add("invalid");
+	}
+
+	function hideError() {
+		const invalidBlock = document.querySelector(".invalid");
+
+		if (invalidBlock) {
+			invalidBlock.classList.remove("invalid");
+		}
+	}
+
+	function tickSlide() {
+		const slides = document.querySelectorAll(".game__slide");
+		const slidesCount = slides.length;
+
+		let currentSlide = 1;
+
+		return () => {
+			currentSlide++;
+			const activeSlide = document.querySelector(".active-slide");
+			activeSlide.classList.remove("active-slide");
+			slides[currentSlide - 1].classList.add("active-slide");
+		};
+	}
+
+	function increaseSlidesCounter() {}
 
 	const isGamePage = () => {
 		const regExp = /game/g;
 		return regExp.test(window.location.pathname);
-	}
+	};
 
 	if (isGamePage()) {
-		startGame(); 
+		startGame();
 	}
-
-
-
-
-
 });
